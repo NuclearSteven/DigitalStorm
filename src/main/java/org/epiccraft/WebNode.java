958971e8dc5daebad0bfed71bd594bfb76daafc3
iNevet <inevet@163.com>
2016-03-27 18:18:16 +0800

    update

--- src/main/java/org/epiccraft/WebNode.java
+++ src/main/java/org/epiccraft/WebNode.java
@@ -1,7 +1,31 @@
 package org.epiccraft;
 
+import org.epiccraft.runtime.exception.ConfigInvalidException;
+import org.epiccraft.runtime.network.NodeNetworkManager;
+
 /**
  * Project WebNode
  */
 public class WebNode {
+
+    private NodeConfig config;
+    private NodeNetworkManager networkManager;
+
+    public WebNode(NodeConfig nodeConfig) {
+        config = nodeConfig;
+        initialize();
+    }
+
+    private void initialize() {
+        networkManager = new NodeNetworkManager(this);
+    }
+
+    public static WebNode create(NodeConfig nodeConfig) throws ConfigInvalidException {
+        if (!nodeConfig.checkValid()) {
+            throw new ConfigInvalidException();
+        }
+
+        return new WebNode(nodeConfig);
+    }
+
 }
